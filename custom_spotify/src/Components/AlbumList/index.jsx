227fc3412a11156a6713944f0440b74e2ffb72ba
Ram <ramswarup.mishra@sakhatech.com>
2019-06-10 01:10:16 +0530

    Implemented till albums, pending song list and player

--- custom_spotify/src/Components/AlbumList/index.jsx
+++ custom_spotify/src/Components/AlbumList/index.jsx
@@ -1,4 +1,4 @@
-import React,{ Component } from 'react';
+import React,{ Component,Fragment } from 'react';
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import {getAlbum} from './../../reducer/album';
@@ -9,10 +9,24 @@ class AlbumList extends Component{
   render(){
     console.log(this.props.data)
     return (
-      
       <div>
         {this.props.data &&
-          <div>This is albumList.</div>}
+          this.props.data.items && <Fragment>
+            <span>Albums</span>
+            <div className="d-flex flex-row flex-wrap">
+              { this.props.data.items.map((item)=>
+                (
+                  <div className=" d-flex p-2" style={{'width':'33%'}}>
+                    <div>
+                      <img style={{height:'100%', width:'100%'}} src={item.images[0].url}/>
+                    </div>
+                  </div>
+                )
+              )
+              }
+            </div>
+          </Fragment>
+        }
       </div>
     )
   }
--- custom_spotify/src/Components/AlbumMenu/index.jsx
+++ custom_spotify/src/Components/AlbumMenu/index.jsx
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import {getAlbum} from './../../reducer/album';
 import AlbumList from './../AlbumList'
+import './AlbumMenu.css';
 
 class AlbumMenu extends Component{
   
@@ -10,15 +11,15 @@ class AlbumMenu extends Component{
     return (
       <div>
         {this.props.artistData &&
-          <div className="col-12">
+          <div className="col-12 p-0">
             <img style={{width: '100%', height: '260px',objectFit:'cover'}} src={this.props.artistData.images[0].url}></img>
           </div>
         }
-        <div className="col-6">
+        <div className="col-6 p-0 customScroll" style={{backgroundColor:'#E8DCFE'}}>
           <AlbumList/>
-          
+
         </div>
-        <div className="col-6">
+        <div className="col-6" style={{backgroundColor:'#E8DCFE'}}>
         </div>
       </div>
     )
